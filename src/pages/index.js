ec120c1353c5e0478af64d35778e2911c0b98b97
Pavel Sidorovich <pavsid2001@mail.ru>
2022-07-03 14:33:39 +0300

    add page exports

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,5 @@ export { default as SignUp } from "./Auth/sign-up";
 export { default as AccountDetails } from "./AccountDetails/account-details";
 export { default as ProductDetails } from "./ProductDetails/product-details";
 export { default as Cart } from "./Cart/cart";
+export { default as ProductControl } from "./ProductControl/product-control";
+export { default as Error } from "./Error/error";
